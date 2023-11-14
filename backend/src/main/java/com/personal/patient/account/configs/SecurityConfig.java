d5a6ed784d3f298b3d60ecfea77926c6d008a6b2
Takudzwa Mupanesure <zhout1709@hotmail.com>
2023-11-14 18:03:15 +0100

    Cors spring security 2 fix

--- backend/src/main/java/com/personal/patient/account/configs/SecurityConfig.java
+++ backend/src/main/java/com/personal/patient/account/configs/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf().disable()
-                .cors().configurationSource(corsConfigurationSource())
+                .cors()
                 .and()
                 .authorizeRequests()
                 .antMatchers("/secured").authenticated()
