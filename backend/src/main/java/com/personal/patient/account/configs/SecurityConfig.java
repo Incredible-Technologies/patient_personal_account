4e7c2523db99f77d1dc403d1370c377d81c816fd
DanilPomortsev <danul78969@gmail.com>
2023-11-14 14:51:33 +0300

    able to activate CORS

--- backend/src/main/java/com/personal/patient/account/configs/SecurityConfig.java
+++ backend/src/main/java/com/personal/patient/account/configs/SecurityConfig.java
@@ -28,7 +28,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf().disable()
-                .cors().disable()
                 .authorizeRequests()
                 .antMatchers("/secured").authenticated()
                 .antMatchers("/info").authenticated()
