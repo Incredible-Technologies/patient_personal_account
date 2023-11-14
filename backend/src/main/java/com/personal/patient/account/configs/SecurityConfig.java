165cbd8362578a6f632fcc38b3944014c757f10f
Takudzwa Mupanesure <zhout1709@hotmail.com>
2023-11-14 19:57:13 +0300

    Cors spring security fix

--- backend/src/main/java/com/personal/patient/account/configs/SecurityConfig.java
+++ backend/src/main/java/com/personal/patient/account/configs/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf().disable()
-                .cors()
+                .cors().configurationSource(corsConfigurationSource())
                 .and()
                 .authorizeRequests()
                 .antMatchers("/secured").authenticated()
@@ -58,8 +58,9 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
 	CorsConfiguration configuration = new CorsConfiguration();
-	configuration.setAllowedOrigins(Arrays.asList("https://lkpatsienta.tech", "http://localhost:4200"));
+	configuration.setAllowedOrigins(Arrays.asList("*"));
 	configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
 	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 	source.registerCorsConfiguration("/**", configuration);
 	return source;
